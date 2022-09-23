4b3c14e2cb7f22ccc87ad3511827c128478411b4
feretto <facundocapurata@gmail.com>
2022-09-23 15:53:59 +0200

    add files

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokedex</title>
+</head>
+
+<body>
+    <?php
+
+    $pokemon = array(
+        "number" =>  $number,
+        "name" => $name,
+        "region" => $region,
+        "type" => $type,
+        "height" => $height,
+        "weight" => $weight,
+        "evolution" => $evolution,
+        "image" => $image
+    );
+
+    ?>
+</body>
+
+</html>
