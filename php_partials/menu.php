227a09711e8b277a837524ef1be25cbfd17260fe
feretto <facundocapurata@gmail.com>
2022-10-20 13:31:45 +0200

    add pokemon_list

--- php_partials/menu.php
+++ php_partials/menu.php
@@ -1,21 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="bootstrap-5.2.2-dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style/style.css">
-    <title>Pokedex</title>
-</head>
-
-<body>
-    <header>
         <nav class="sticky-top navbar-expand-lg navbar navbar-dark bg-primary">
             <div class="container-fluid">
 
-                <a class="navbar-brand" href="index.php"><img src="media/pokedex.png" class="me-1" id="imageNav">Pokedex</a>
+                <a class="navbar-brand" href="/pokedex/index.php"><img src="/pokedex/media/pokedex.png" class="me-1" id="imageNav">Pokedex</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -23,11 +9,11 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle" href="/pokedex/php_views/pokemon_list.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Master Data
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="pokemon_list.php">Pokemons</a></li>
+                                <li><a class="dropdown-item" href="/pokedex/php_views/pokemon_list.php">Pokemons</a></li>
                                 <!-- <hr class="dropdown-divider"> -->
                             </ul>
                         </li>
@@ -35,15 +21,3 @@
                 </div>
             </div>
         </nav>
-    </header>
-    <section>
-
-    </section>
-    <footer>
-
-    </footer>
-</body>
-<script src=" ./bootstrap-5.2.2-dist/js/bootstrap.bundle.min.js">
-</script>
-
-</html>
